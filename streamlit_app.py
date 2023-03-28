5a35730fd928db3800e5cc5b1fe7672eb986d544
MainBen01 <128146319+MainBen01@users.noreply.github.com>
2023-03-28 13:32:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
    streamlit.dataframe(fruityvice_normalized)
     
 except URLError as e:
-   streamlit.error()
+   streamlit.error('Please select a fruit to get information')
    
    # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like add?','Jackfruit')
